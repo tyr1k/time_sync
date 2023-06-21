988f9226d9e751a1d8b94673b5b0b8c7f3136122
tyr1k <65919376+tyr1k@users.noreply.github.com>
2023-06-21 10:36:51 +0300

    Update time_sync.sh

--- time_sync.sh
+++ time_sync.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#Скрипт для автоматической синхронизации времени при разнице в 1 час
-
 # Значения IP-адреса и имени пользователя удаленной машины
-remote_host="172.29.100.2"
-remote_user="mtrust"
+remote_host="ip_address"
+remote_user="username"
 
 # Функция для проверки разницы во времени
 check_time_difference() {
     local remote_time=$(ssh $remote_user@$remote_host date +%s)
     local local_time=$(date +%s)
-    local time_difference=$((remote_time - local_time))
+    local time_difference=$(echo $((remote_time - local_time)) | tr -d '-')
+
+    echo $time_difference
 
     # Проверяем, превышает ли разница во времени 1 час (3600 секунд)
     if [ $time_difference -gt 3600 ]; then
         # Выставляем время на удаленной машине с помощью команды date
-        ssh $remote_user@$remote_host date -s "$(date +"%Y-%m-%d %H:%M:%S")"
+        ssh $remote_user@$remote_host "sudo date -s '$(date +"%Y-%m-%d %T")'"
         echo "Время на удаленной машине было обновлено."
     else
         echo "Разница во времени между клиентской и удаленной машинами не превышает 1 час."
